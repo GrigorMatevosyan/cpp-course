4de75f9b00a9fa5311354a5a6042bc1784f22894
Grigor Matevosyan <grigor.matevosyan@basic.local>
2021-05-01 20:35:35 +0400

    first lesson

---
+++ lesson1/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+/*int count = 255;
+cout<<count;*/
+
+/*int count;
+cin>>count;
+cout<<count;*/
+
+  int count, age;
+  cout<<"Enter your number "<<" ";
+  cin>>count;
+  cout<<"Enter your age ";
+  cin>>age;
+  return 0;
+}
