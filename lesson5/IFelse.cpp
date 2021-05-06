22e7214c0a394f531ce687a6a2f85c8b5d321d20
GrigorMatevosyan <83417294+GrigorMatevosyan@users.noreply.github.com>
2021-05-06 18:53:33 +0400

    If Else If Else

---
+++ lesson5/IFelse.cpp
@@ -0,0 +1,15 @@
+int main() {
+int point;
+ cout << "point is equal ";
+ cin >> point;
+ if(point >= 18 ){
+   cout<< point << " A";
+ } else if(point >=15){
+ cout<< point << " B";
+ }else if(point >=8){
+   cout<< point << " C";
+ } else{
+  cout<< point << " D" << endl;
+ }
+return 0;
+}
