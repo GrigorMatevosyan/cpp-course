1f1f99b4d3a37bcd68bfb258c9a076c4a8813d08
GrigorMatevosyan <83417294+GrigorMatevosyan@users.noreply.github.com>
2021-05-06 18:35:27 +0400

    Create plural-sing.cpp

---
+++ lesson5/plural-sing.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+int a;
+ cin>> a;
+ if(a % 2 == 0){
+   cout<< a << "tiv@ zuyg e";
+
+ } else{
+ cout<< a << "tiv@ zuyg  che";
+  
+ }
+return 0;
+}
